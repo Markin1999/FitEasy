8483c865d0a06650c5105244e12a50ff57c12f17
Marco Filannino <filanninobusiness@gmail.com>
2025-06-18 16:12:22 +0200

    Aggiunto Header, tailwind e react router

---
+++ vite-project/src/componenti/homepage/header.jsx
@@ -0,0 +1,21 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <h1>
+          L'app per personal trainer n. 1 per coinvolgere al meglio i tuoi
+          clienti.
+        </h1>
+        <p>
+          Porta il tuo lavoro da coach al livello successivo. Con FitEasy puoi
+          gestire clienti, creare schede di allenamento e alimentazione
+          personalizzate, monitorare i progressi e automatizzare tutto da
+          un’unica piattaforma. Offri un’esperienza professionale, risparmia
+          tempo e concentra le tue energie dove contano davvero: sui risultati
+          dei tuoi clienti.
+        </p>
+      </div>
+      <video src=""></video>
+    </header>
+  );
+}
