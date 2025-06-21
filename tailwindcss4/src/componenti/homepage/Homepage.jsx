045f891deaefcfc7824aaf176b7c63d176008c78
Marco Filannino <filanninobusiness@gmail.com>
2025-06-21 17:12:01 +0200

    Inserito logo nella navbar

--- tailwindcss4/src/componenti/homepage/Homepage.jsx
+++ tailwindcss4/src/componenti/homepage/Homepage.jsx
@@ -3,9 +3,9 @@ import { Navbar } from "./navbar";
 
 export function Homepage() {
   return (
-    <>
+    <div className="m-0">
       <Navbar />
       <Header />
-    </>
+    </div>
   );
 }
--- tailwindcss4/src/componenti/homepage/navbar.jsx
+++ tailwindcss4/src/componenti/homepage/navbar.jsx
@@ -1,8 +1,12 @@
 export function Navbar() {
   return (
-    <nav className="bg-testo h-16">
-      <div>
-        <img src="../../src/assets/logo/logo.svg" alt="" />
+    <nav className="bg-testo p-3">
+      <div className="h-[90%]">
+        <img
+          src="../../src/assets/logo/logo.svg"
+          className="ml-3 w-[173px] h-[80px]"
+          alt=""
+        />
       </div>
     </nav>
   );
