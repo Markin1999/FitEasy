0824bd0ee09042b768f7def56d157d2ff94f8932
Marco Filannino <filanninobusiness@gmail.com>
2025-07-04 22:47:10 +0200

    sistemata navbar

--- tailwindcss4/src/componenti/homepage/navbar.jsx
+++ tailwindcss4/src/componenti/homepage/navbar.jsx
@@ -1,19 +1,19 @@
 export function Navbar() {
   return (
-    <nav className="bg-testo p-3 gap-2">
+    <nav className="bg-testo py-3 px-12 gap-2">
       <div className="flex justify-between">
-        <div className="h-[90%] ">
+        <div className="h-max ">
           <img
             src="../../src/assets/logo/logo.svg"
-            className="w-[173px] h-[80px] items-center"
+            className="h-[80px] items-center"
             alt=""
           />
         </div>
 
-        <div className="flex items-center gap-4">
-          <a>Accedi</a>
-          <button className=" font-bold bg-bottone text-white rounded-2xl px-4 py-2">
-            Iscriviti ora
+        <div className="flex items-center gap-4 hover:cursor-pointer">
+          <a className=" text-titolo">ACCEDI</a>
+          <button className="p-5 font-bold bg-bottone text-white rounded-md inline-block">
+            ISCRIVITI ORA
           </button>
         </div>
       </div>
