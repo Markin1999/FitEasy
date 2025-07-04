49a792bd6cc4e37ffe88a4f304ab2eb3dbc351a2
Marco Filannino <filanninobusiness@gmail.com>
2025-07-04 20:55:17 +0200

    Inizializzato server

---
+++ tailwindcss4/server/server.js
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`server in ascolto su http://localhost:${PORT}`);
+});
