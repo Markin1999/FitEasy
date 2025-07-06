08d901a23a8301ba5b044be6adf85719d73ee273
Marco Filannino <filanninobusiness@gmail.com>
2025-07-06 15:19:21 +0200

    Creata tabella su pgAdmin

---
+++ tailwindcss4/server/initDb.js
@@ -0,0 +1,29 @@
+import pgPromise from "pg-promise";
+import dotenv from "dotenv";
+
+export const db = pgPromise()(
+  "postgresql://postgres:Lggs290@localhost:5432/FitEasy"
+);
+
+const setup = async () => {
+  try {
+    await db.none(
+      `
+        CREATE TABLE IF NOT EXIST users (
+        id SERIAL PRIMARY KEY,
+        nome TEXT NOT NULL,
+        cognome TEXT NOT NULL,
+        businessName TEXT NOT NULL,
+        businessEmail TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        token TEXT 
+        )
+        `
+    );
+    console.log("Tabelle create correttamente");
+  } catch (error) {
+    console.error("Errore durante la creazione delle tabelle:", error.message);
+  }
+};
+
+setup();
