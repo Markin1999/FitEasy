17358c23370e673fa2c26199f86f831df19fc5ae
Marco Filannino <filanninobusiness@gmail.com>
2025-07-08 01:02:17 +0200

    Aggiunto registrazione, solo html

--- tailwindcss4/src/componenti/registrazione/Registrazione.jsx
+++ tailwindcss4/src/componenti/registrazione/Registrazione.jsx
@@ -1,1 +1,41 @@
-export default function Registrazione() {}
+export default function Registrazione() {
+  return (
+    <div className="flex ">
+      <div>
+        <h1>INIZIA LA TUA PROVA GRATUITA DI 30 GIORNI</h1>
+        <p>Incrementa i tuoi clienti con Fit-east.</p>
+        <p>Nessuna carta di credito richiesta</p>
+      </div>
+      <form>
+        <div>
+          <input
+            type="text"
+            className="w-[65%]  border border-testo hover:border-testo rounded-l-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
+          ></input>
+          <input
+            type="text"
+            className="w-[65%]  border border-testo hover:border-testo rounded-l-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
+          ></input>
+        </div>
+        <input
+          type="text"
+          className="w-[65%]  border border-testo hover:border-testo rounded-l-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
+        ></input>
+        <input
+          type="email"
+          className="w-[65%]  border border-testo hover:border-testo rounded-l-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
+        ></input>
+        <input
+          type="password"
+          className="w-[65%]  border border-testo hover:border-testo rounded-l-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
+        ></input>
+        <button></button>
+      </form>
+
+      <p>
+        Cliccando su Crea account e registrandoti, accetti i nostri termini e
+        condizioni e la privacy police.
+      </p>
+    </div>
+  );
+}
