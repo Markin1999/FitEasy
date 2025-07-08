4df59106d39b5f8a4ec0f17c3fd4e18bde50faed
Marco Filannino <filanninobusiness@gmail.com>
2025-07-08 22:37:39 +0200

    Sistemate altre cose alla grafica di registrazione. Disponibile per tutti i dispositivi ora

--- tailwindcss4/src/componenti/registrazione/Registrazione.jsx
+++ tailwindcss4/src/componenti/registrazione/Registrazione.jsx
@@ -1,17 +1,21 @@
 export default function Registrazione() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center px-3">
+    <div className="flex flex-col h-screen justify-center items-center px-3 my-3">
       <div className="text-center p-3 sm:mb-12 ">
-        <h2 className="font-bold text-titolo mb-2 sm:mb-6">
+        <h2 className="font-bold text-testo mb-2 sm:mb-6">
           INIZIA LA PROVA GRATUITA <br /> DI 30 GIORNI
         </h2>
-        <h3 className="mb-[0.1em]">Incrementa i tuoi clienti con Fit-easy.</h3>
+        <h3 className="mb-[0.1em] sm:mb-[0.5em]">
+          Incrementa i tuoi clienti con Fit-easy.
+        </h3>
         <h3>Nessuna carta di credito richiesta</h3>
       </div>
-      <form className="flex flex-col w-full items-center gap-3 sm:w-[75%] lg:w-[30%]">
-        <div className="flex w-full gap-5">
+      <form className="flex flex-col w-full items-center gap-3 sm:w-[75%] lg:w-[40%]">
+        <div className="flex w-full gap-5 sm:flex-col">
           <div className="flex flex-col w-full">
-            <label htmlFor="nome">NOME</label>
+            <label htmlFor="nome" className="font-semibold text-testo">
+              NOME
+            </label>
             <input
               type="text"
               className="w-full  border border-testo hover:border-testo rounded-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
@@ -19,16 +23,20 @@ export default function Registrazione() {
           </div>
 
           <div className="flex flex-col w-full">
-            <label htmlFor="cognome">COGNOME</label>
+            <label htmlFor="cognome" className="font-semibold text-testo">
+              COGNOME
+            </label>
             <input
               type="text"
-              className="w-full  border border-testo hover:border-testo rounded-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
+              className="w-full   border border-testo hover:border-testo rounded-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
             ></input>
           </div>
         </div>
 
         <div className="flex flex-col w-full">
-          <label htmlFor="nomeAttivita">NOME ATTIVITA</label>
+          <label htmlFor="nomeAttivita" className="font-semibold text-testo">
+            NOME ATTIVITA
+          </label>
           <input
             type="text"
             className="w-full  border border-testo hover:border-testo rounded-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
@@ -36,7 +44,9 @@ export default function Registrazione() {
         </div>
 
         <div className="flex flex-col w-full">
-          <label htmlFor="email">EMAIL AZIENDALE</label>
+          <label htmlFor="email" className="font-semibold text-testo">
+            EMAIL AZIENDALE
+          </label>
           <input
             type="email"
             className="w-full  border border-testo hover:border-testo rounded-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
@@ -44,14 +54,16 @@ export default function Registrazione() {
         </div>
 
         <div className="flex flex-col w-full">
-          <label htmlFor="password">PASSWORD</label>
+          <label htmlFor="password" className="font-semibold text-testo">
+            PASSWORD
+          </label>
           <input
             type="password"
             className="w-full   border border-testo hover:border-testo rounded-md px-4 py-2 text-[12px] sm:text-[14px] lg:text-[16px] lg:h-16 "
           ></input>
         </div>
 
-        <button className="w-full mb-3 border border-bottone bg-bottone text-white rounded-md px-4 py-2 sm:my-6 ">
+        <button className="w-full font-bold my-3 border border-bottone bg-bottone text-white rounded-md px-4 py-2 sm:my-6 sm:py-4 lg:py-6 ">
           Crea Account
         </button>
       </form>
