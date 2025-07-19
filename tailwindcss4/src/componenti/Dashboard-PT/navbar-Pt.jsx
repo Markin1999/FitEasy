8d570cb1f72433558e56c2ded841f61ecdb90ca9
Marco Filannino <filanninobusiness@gmail.com>
2025-07-19 20:49:21 +0200

    Termitata navar pt dashboard in modo responsive

--- tailwindcss4/src/componenti/Dashboard-PT/navbar-Pt.jsx
+++ tailwindcss4/src/componenti/Dashboard-PT/navbar-Pt.jsx
@@ -10,22 +10,22 @@ export function NavbarPt() {
   };
 
   return (
-    <nav className="bg-testo gap-2 px-5 py-3 fixed w-full sm:px-12  ">
+    <nav className="bg-testo gap-2 px-5 py-2  fixed w-full sm:px-12 md:py-0 lg:py-3 ">
       <div className="flex">
-        <div className="hidden w-full justify-center items-center gap-4 hover:cursor-pointer md:grid md:grid-cols-3">
+        <div className="w-full justify-center items-center gap-4 md:grid md:grid-cols-3  ">
           <div className="h-full ">
             <img
               src="../../src/assets/logo/logo.svg"
-              className="h-[60px] md:h-[80px]"
+              className="h-[60px] w-[60px] md:h-[80px] md:w-[60px] lg:w-[90px] "
               alt=""
             />
           </div>
-          <ul className="flex justify-center text-white gap-8  ">
-            <li>Dashboard</li>
-            <li>Clienti</li>
-            <li>Calendario</li>
-            <li>Notifiche</li>
-            <li>Profilo</li>
+          <ul className="hidden justify-center text-white md:gap-5 md:flex lg:gap-8  ">
+            <li>DASHBOARD</li>
+            <li>CLIENTI</li>
+            <li>CALENDARIO</li>
+            <li>NOTIFICHE</li>
+            <li>PROFILO</li>
           </ul>
           <div></div>
         </div>
@@ -50,25 +50,16 @@ export function NavbarPt() {
         </div>
       </div>
       {clickBurger && (
-        <div className="flex flex-col sm:hidden py-4">
+        <div className="flex flex-col">
           <hr className="pb-4 text-sfondo" />
-          <div className="flex flex-col items-end gap-4 hover:cursor-pointer">
-            <a
-              className=" text-titolo pr-4"
-              onClick={() => {
-                navTo("/accedi");
-              }}
-            >
-              ACCEDI
-            </a>
-            <button
-              onClick={() => {
-                navTo("/registrazione");
-              }}
-              className="p-3 m-0 font-bold bg-bottone text-white rounded-md inline-block"
-            >
-              ISCRIVITI ORA
-            </button>
+          <div className="flex flex-col items-end ">
+            <ul className=" text-white text-right">
+              <li className="py-0.5">DASHBOARD</li>
+              <li className="py-0.5">CLIENTI</li>
+              <li className="py-0.5">CALENDARIO</li>
+              <li className="py-0.5">NOTIFICHE</li>
+              <li className="py-0.5">PROFILO</li>
+            </ul>
           </div>
         </div>
       )}
